6389590b6b260a1e67112d67db05029794c0f357
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-24 14:41:18 +0600

    addEventListener

--- events/events.js
+++ events/events.js
@@ -18,3 +18,21 @@ const makePurpleButton = document.getElementById('make-purple');
 makePurpleButton.onclick = function makePurple(){
     document.body.style.backgroundColor = 'purple';
 }
+
+// option: 4
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+// option: 4 another
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+});
+// option: 4 Final
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod';
+})
